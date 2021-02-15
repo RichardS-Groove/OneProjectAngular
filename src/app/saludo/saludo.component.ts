a0052bef9d4aed067c6caf8ce84428cc92396f39
RichardS <richardsjcb@gmail.com>
2021-02-15 12:22:11 -0300

    Se agrega componente Saludo

---
+++ src/app/saludo/saludo.component.ts
@@ -0,0 +1,17 @@
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+
+@Component({
+  selector: 'saludo',
+  templateUrl: './saludo.component.html',
+  styleUrls: ['./saludo.component.css']
+})
+export class SaludoComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+}
